9d9f002f68d9429258d8bdee5d9005f32b07bfc1
Abu Desai <abhyuday.desai@gmail.com>
2022-09-16 22:47:46 -0700

    update to use test_key

--- local_test/run_local.py
+++ local_test/run_local.py
@@ -139,31 +139,31 @@ def load_and_test_algo():
     # save predictions
     utils.save_dataframe(predictions, testing_outputs_path, "test_predictions.csv")
     # score the results
-    results = score(test_data, predictions, data_schema)  
-    # local explanations
-    local_explanations = None
-    if hasattr(predictor, "has_local_explanations"): 
-        # will only return explanations for max 5 rows - will select the top 5 if given more rows        
-        local_explanations = predictor.explain_local(test_data)        
+    test_key = get_test_key()
+    results = score(test_key, predictions, data_schema)  
     print("done with predictions")
-    return results, local_explanations
+    return results
 
 
-def score(test_data, predictions, data_schema): 
+def get_test_key():
+    test_key = pd.read_csv(f"{local_datapath}/{dataset_name}/{dataset_name}_test_key.csv")
+    return test_key
+
+
+def score(test_key, predictions, data_schema): 
     # we need to get a couple of field names in the test_data file to do the scoring 
     # we get it using the schema file
     id_field = data_schema["inputDatasets"]["multiClassClassificationBaseMainInput"]["idField"]
     target_field = data_schema["inputDatasets"]["multiClassClassificationBaseMainInput"]["targetField"]
-        
-    pred_class_names = [ str(c) for c in predictions.columns[1:]    ]   
-    predictions.columns = [str(c) for c in list(predictions.columns)]    
+            
+    pred_class_names = [ c for c in predictions.columns[1:]    ]  
     
     predictions["__pred_class"] = pd.DataFrame(predictions[pred_class_names], columns = pred_class_names).idxmax(axis=1)  
-    predictions = predictions.merge(test_data[[id_field, target_field]], on=[id_field])
-    pred_probabilities = predictions[pred_class_names].copy()
+    predictions = predictions.merge(test_key[[id_field, target_field]], on=[id_field])
+    predictions = predictions[predictions[target_field].isin(pred_class_names)]    
     
     Y = predictions[target_field].astype(str)
-    Y_hat = predictions["__pred_class"].astype(str)   
+    Y_hat = predictions["__pred_class"].astype(str)    
     
     accu = accuracy_score(Y , Y_hat)  
     f1 = f1_score(Y , Y_hat, average='weighted')    
@@ -174,8 +174,8 @@ def score(test_data, predictions, data_schema):
     name_to_idx_dict = {str(n):i for i,n in enumerate(pred_class_names)}
     mapped_classes_true = Y.map(name_to_idx_dict)     
     
-    auc = roc_auc_score(mapped_classes_true, pred_probabilities[pred_class_names].values, 
-        labels=np.arange(len(pred_class_names)), average='weighted', multi_class='ovo')     
+    auc = roc_auc_score(mapped_classes_true, predictions[pred_class_names].values, 
+        labels=np.arange(len(pred_class_names)), average='weighted', multi_class='ovo')   
     
     # -------------------------------------   
     scores = { 
@@ -184,7 +184,7 @@ def score(test_data, predictions, data_schema):
                "precision": np.round(precision, 4), 
                "recall": np.round(recall, 4), 
                "auc_score": np.round(auc, 4), 
-               "perc_pred_missing": np.round( 100 * (1 - predictions.shape[0] / test_data.shape[0]), 2)
+               "perc_pred_missing": np.round( 100 * (1 - predictions.shape[0] / test_key.shape[0]), 2)
                }
     return scores
 
@@ -193,20 +193,14 @@ def save_test_outputs(results, run_hpt, dataset_name):
     df = pd.DataFrame(results) if dataset_name is None else pd.DataFrame([results])        
     df = df[["model", "dataset_name", "run_hpt", "num_hpt_trials", 
              "accuracy", "f1_score", "precision", "recall", "auc_score", "perc_pred_missing",
-             "elapsed_time_in_minutes"]]    
-    print(df)    
+             "elapsed_time_in_minutes"]]
+    
+    print(df)
+    
     file_path_and_name = get_file_path_and_name(run_hpt, dataset_name)
     df.to_csv(file_path_and_name, index=False)
     
 
-def save_local_explanations(local_explanations, dataset_name): 
-    if local_explanations is not None: 
-        fname = f"{model_name}_{dataset_name}_local_explanations.json"
-        file_path_and_name = os.path.join(test_results_path, fname)
-        with open(file_path_and_name, 'w') as f:
-            f.writelines(local_explanations)
-        
-        
 def get_file_path_and_name(run_hpt, dataset_name): 
     if dataset_name is None: 
         fname = f"_{model_name}_results_with_hpt.csv" if run_hpt else f"_{model_name}_results_no_hpt.csv"
@@ -225,7 +219,7 @@ def run_train_and_test(dataset_name, run_hpt, num_hpt_trials):
     train_and_save_algo()        # train the model and save
     
     # set_scoring_vars(dataset_name=dataset_name)
-    results, local_explanations  = load_and_test_algo()        # load the trained model and get predictions on test data
+    results = load_and_test_algo()        # load the trained model and get predictions on test data
     
     end = time.time()
     elapsed_time_in_minutes = np.round((end - start)/60.0, 2)
@@ -239,26 +233,25 @@ def run_train_and_test(dataset_name, run_hpt, num_hpt_trials):
                }
     
     print(f"Done with dataset in {elapsed_time_in_minutes} minutes.")
-    return results, local_explanations 
+    return results
 
 
 if __name__ == "__main__": 
     
-    num_hpt_trials = 10
+    num_hpt_trials = 30
     run_hpt_list = [False, True]
-    run_hpt_list = [False]
+    # run_hpt_list = [False]
     
     datasets = ["car", "primary_tumor", "splice", "statlog", "steel_plate_fault", "wine"]
-    datasets = ["statlog"]
+    # datasets = ["primary_tumor"]
     
     for run_hpt in run_hpt_list:
         all_results = []
         for dataset_name in datasets:        
             print("-"*60)
             print(f"Running dataset {dataset_name}")
-            results, local_explanations = run_train_and_test(dataset_name, run_hpt, num_hpt_trials)
-            save_test_outputs(results, run_hpt, dataset_name)   
-            save_local_explanations(local_explanations, dataset_name)            
+            results = run_train_and_test(dataset_name, run_hpt, num_hpt_trials)
+            save_test_outputs(results, run_hpt, dataset_name)            
             all_results.append(results)
             print("-"*60)
         
