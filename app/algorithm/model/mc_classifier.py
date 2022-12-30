c54e2e394608f6ae8e6afaaa0709d21b63415518
Abu Desai <abhyuday.desai@gmail.com>
2022-12-29 21:57:14 -0800

    test changed to predict; added json inference; user 1000 in dockerfile; nginx chown in dockerfile

--- app/algorithm/model/mc_classifier.py
+++ app/algorithm/model/mc_classifier.py
@@ -1,10 +1,10 @@
-
 import numpy as np, pandas as pd
 import joblib
 import sys
 import os, warnings
 import matplotlib.pyplot as plt
-warnings.filterwarnings('ignore') 
+
+warnings.filterwarnings("ignore")
 
 from interpret.glassbox import ExplainableBoostingClassifier as EBC
 
@@ -16,107 +16,106 @@ global_explanations_chart_fname = "global_explanations.png"
 MODEL_NAME = "multi_class_base_ebm_interpret"
 
 
-class Classifier(): 
-    
-    def __init__(self, feature_names, min_samples_leaf= 2, learning_rate= 1e-3,  **kwargs) -> None:
+class Classifier:
+    def __init__(
+        self, feature_names, min_samples_leaf=2, learning_rate=1e-3, **kwargs
+    ) -> None:
 
         self.feature_names = feature_names
-        self.min_samples_leaf= min_samples_leaf
-        self.learning_rate= learning_rate
+        self.min_samples_leaf = min_samples_leaf
+        self.learning_rate = learning_rate
         self.MAX_LOCAL_EXPLANATIONS = 5
-        
-        self.model = self.build_model()     
-        
-        
-    def build_model(self): 
+
+        self.model = self.build_model()
+
+    def build_model(self):
 
         model = EBC(
-            feature_names = self.feature_names, 
-            min_samples_leaf= self.min_samples_leaf,
-            learning_rate= self.learning_rate,
-            random_state=0)
+            feature_names=self.feature_names,
+            min_samples_leaf=self.min_samples_leaf,
+            learning_rate=self.learning_rate,
+            random_state=0,
+        )
         return model
-    
-    
-    def fit(self, train_X, train_y):        
-        self.model.fit(
-            X= train_X, 
-            y= train_y)    
-        
-        
-    def explain_global(self, name): 
+
+    def fit(self, train_X, train_y):
+        self.model.fit(X=train_X, y=train_y)
+
+    def explain_global(self, name):
         return self.model.explain_global(name=name)
-    
-    def predict(self, X, verbose=False): 
+
+    def predict(self, X, verbose=False):
         preds = self.model.predict(X)
-        return preds 
-    
-    def predict_proba(self, X, verbose=False): 
+        return preds
+
+    def predict_proba(self, X, verbose=False):
         preds = self.model.predict_proba(X)
-        return preds 
-    
+        return preds
 
     def summary(self):
         self.model.get_params()
-        
-    
-    def evaluate(self, x_test, y_test): 
+
+    def evaluate(self, x_test, y_test):
         """Evaluate the model and return the loss and metrics"""
         if self.model is not None:
-            return self.model.score(x_test, y_test)       
+            return self.model.score(x_test, y_test)
 
-    
-    def save(self, model_path): 
+    def save(self, model_path):
         joblib.dump(self, os.path.join(model_path, model_fname))
         self._save_global_explanations(model_path=model_path)
-        
 
-    def explain_local(self, X):  
+    def explain_local(self, X):
         local_explanations = self.model.explain_local(X=X, y=None)
         return local_explanations
 
-
     def _save_global_explanations(self, model_path):
-        global_explanations = self.model.explain_global(name="explainable-boosting-machine")
+        global_explanations = self.model.explain_global(
+            name="explainable-boosting-machine"
+        )
         data = global_explanations.data()
         df = pd.DataFrame()
         # create list of feature names - "extra" contains the intercept
-        # read documentation of interpret ml global_explanations for details        
-        df['feature'] = data['names'] + [ data['names'][0] ]
-        df['score'] = data['scores'] + [ data['scores'][0] ]      
-        df.sort_values(by=['score'], inplace=True, ascending=False)
-        df.to_csv(os.path.join(model_path, global_explanations_fname), index=False, float_format='%.4f')
-        save_plot_of_explanations(df['score'], df['feature'], model_path)
-        
-
+        # read documentation of interpret ml global_explanations for details
+        df["feature"] = data["names"] + [data["names"][0]]
+        df["score"] = data["scores"] + [data["scores"][0]]
+        df.sort_values(by=["score"], inplace=True, ascending=False)
+        df.to_csv(
+            os.path.join(model_path, global_explanations_fname),
+            index=False,
+            float_format="%.4f",
+        )
+        save_plot_of_explanations(df["score"], df["feature"], model_path)
 
     @classmethod
-    def load(cls, model_path): 
+    def load(cls, model_path):
         model = joblib.load(os.path.join(model_path, model_fname))
         return model
 
 
-def save_model(model, model_path):    
-    model.save(model_path) 
-    
+def save_model(model, model_path):
+    model.save(model_path)
 
-def load_model(model_path): 
-    try: 
-        model = Classifier.load(model_path)        
-    except: 
-        raise Exception(f'''Error loading the trained {MODEL_NAME} model. 
-            Do you have the right trained model in path: {model_path}?''')
+
+def load_model(model_path):
+    try:
+        model = Classifier.load(model_path)
+    except:
+        raise Exception(
+            f"""Error loading the trained {MODEL_NAME} model. 
+            Do you have the right trained model in path: {model_path}?"""
+        )
     return model
 
 
-
-def save_plot_of_explanations(vals, labels, model_path):    
-    height = 2. + len(vals)*0.3
-    colors = ['rosybrown' if x < 0 else 'steelblue' for x in vals]
-    plt.figure(figsize=(16, height), dpi=80) 
-    plt.barh(labels,vals, color=colors)  
-    plt.xlabel('score', fontsize=18); plt.ylabel('feature', fontsize=16)
-    plt.yticks(labels, fontsize=14); plt.xticks(fontsize=14)
-    plt.title('Global Feature Impact', fontdict={ 'size': 20})
-    plt.grid(linestyle='--', alpha=0.5)
+def save_plot_of_explanations(vals, labels, model_path):
+    height = 2.0 + len(vals) * 0.3
+    colors = ["rosybrown" if x < 0 else "steelblue" for x in vals]
+    plt.figure(figsize=(16, height), dpi=80)
+    plt.barh(labels, vals, color=colors)
+    plt.xlabel("score", fontsize=18)
+    plt.ylabel("feature", fontsize=16)
+    plt.yticks(labels, fontsize=14)
+    plt.xticks(fontsize=14)
+    plt.title("Global Feature Impact", fontdict={"size": 20})
+    plt.grid(linestyle="--", alpha=0.5)
     plt.savefig(os.path.join(model_path, global_explanations_chart_fname))
--- app/algorithm/model_server.py
+++ app/algorithm/model_server.py
@@ -7,121 +7,117 @@ import algorithm.preprocessing.pipeline as pipeline
 import algorithm.model.mc_classifier as mc_classifier
 
 
-# get model configuration parameters 
+# get model configuration parameters
 model_cfg = utils.get_model_config()
 
 
 class ModelServer:
-    def __init__(self, model_path, data_schema): 
+    def __init__(self, model_path, data_schema):
         self.model_path = model_path
         self.data_schema = data_schema
         self.preprocessor = None
         self.model = None
         self.has_local_explanations = True
         self.MAX_LOCAL_EXPLANATIONS = 3
-        self.id_field_name = self.data_schema["inputDatasets"]["multiClassClassificationBaseMainInput"]["idField"]  
-       
-    
-    def _get_preprocessor(self): 
-        if self.preprocessor is None: 
-            try: 
-                self.preprocessor = pipeline.load_preprocessor(self.model_path)
-                return self.preprocessor
-            except: 
-                print(f'Could not load preprocessor from {self.model_path}. Did you train the model first?')
-                return None
-        else: return self.preprocessor
-    
-    def _get_model(self): 
-        if self.model is None: 
-            try: 
-                self.model = mc_classifier.load_model(self.model_path)
-                return self.model
-            except: 
-                print(f'Could not load model from {self.model_path}. Did you train the model first?')
-                return None
-        else: return self.model
-        
-    
-    def _get_predictions(self, data, return_probs = True):  
+        self.id_field_name = self.data_schema["inputDatasets"][
+            "multiClassClassificationBaseMainInput"
+        ]["idField"]
+
+    def _get_preprocessor(self):
+        if self.preprocessor is None:
+            self.preprocessor = pipeline.load_preprocessor(self.model_path)
+        return self.preprocessor
+
+    def _get_model(self):
+        if self.model is None:
+            self.model = mc_classifier.load_model(self.model_path)
+        return self.model
+
+    def _get_predictions(self, data, return_probs=True):
         preprocessor = self._get_preprocessor()
         model = self._get_model()
-        
-        if preprocessor is None:  raise Exception("No preprocessor found. Did you train first?")
-        if model is None:  raise Exception("No model found. Did you train first?")
-                    
+
+        if preprocessor is None:
+            raise Exception("No preprocessor found. Did you train first?")
+        if model is None:
+            raise Exception("No model found. Did you train first?")
+
         # transform data - returns a dict of X (transformed input features) and Y(targets, if any, else None)
-        proc_data = preprocessor.transform(data)          
+        proc_data = preprocessor.transform(data)
         # Grab input features for prediction
-        pred_X, pred_ids = proc_data['X'].astype(np.float), proc_data['ids']    
+        pred_X, pred_ids = proc_data["X"].astype(np.float), proc_data["ids"]
         # make predictions
         if return_probs:
-            preds = model.predict_proba( pred_X )
-        else: 
-            preds = model.predict( pred_X )
+            preds = model.predict_proba(pred_X)
+        else:
+            preds = model.predict(pred_X)
         return preds, pred_ids
-    
-    
-    def predict_proba(self, data):          
+
+    def predict_proba(self, data):
         preds, pred_ids = self._get_predictions(data, return_probs=True)
-        class_names = pipeline.get_class_names(self.preprocessor, model_cfg)        
+        class_names = pipeline.get_class_names(self.preprocessor, model_cfg)
         id_df = pd.DataFrame(pred_ids, columns=[self.id_field_name])
-        
-        # return the prediction df with the id and class probability fields        
-        preds_df = pd.concat( [ id_df, pd.DataFrame(preds, columns = class_names)], axis=1 )
-        return preds_df     
-    
-    
-    def predict(self, data):        
-        preds_df = self.predict_proba(data)
-        class_names = [ str(c) for c in preds_df.columns[1:] ]          
-        preds_df["prediction"] = pd.DataFrame(preds_df[class_names], columns = class_names).idxmax(axis=1)     
-        preds_df.drop(class_names, axis=1, inplace=True) 
+
+        # return the prediction df with the id and class probability fields
+        preds_df = pd.concat([id_df, pd.DataFrame(preds, columns=class_names)], axis=1)
         return preds_df
 
+    def predict(self, data):
+        preds_df = self.predict_proba(data)
+        class_names = [str(c) for c in preds_df.columns[1:]]
+        preds_df["prediction"] = pd.DataFrame(
+            preds_df[class_names], columns=class_names
+        ).idxmax(axis=1)
+        preds_df.drop(class_names, axis=1, inplace=True)
+        return preds_df
 
     def explain_local(self, data):
         if data.shape[0] > self.MAX_LOCAL_EXPLANATIONS:
-            msg = f'''Warning!
+            msg = f"""Warning!
             Maximum {self.MAX_LOCAL_EXPLANATIONS} explanation(s) allowed at a time. 
             Given {data.shape[0]} samples. 
-            Selecting top {self.MAX_LOCAL_EXPLANATIONS} sample(s) for explanations.'''
+            Selecting top {self.MAX_LOCAL_EXPLANATIONS} sample(s) for explanations."""
             print(msg)
-        
+
         data = data.head(self.MAX_LOCAL_EXPLANATIONS)
-        print(f"Now generating local explanations for {data.shape[0]} sample(s).")     
+        print(f"Now generating local explanations for {data.shape[0]} sample(s).")
         # ------------------------------------------------------------------------------
-        preprocessor = self._get_preprocessor()        
-        proc_data = preprocessor.transform(data)  
-        pred_X, ids = proc_data['X'].astype(np.float), proc_data['ids']  
-        
+        preprocessor = self._get_preprocessor()
+        proc_data = preprocessor.transform(data)
+        pred_X, ids = proc_data["X"].astype(np.float), proc_data["ids"]
+
         model = self._get_model()
-        
-        local_explanations = model.explain_local(pred_X)        
-        
+
+        local_explanations = model.explain_local(pred_X)
+
         class_names = pipeline.get_class_names(self.preprocessor, model_cfg)
-        all_explanations = []        
+        all_explanations = []
         for i in range(pred_X.shape[0]):
-            local_expl_data = local_explanations.data(i)            
+            local_expl_data = local_explanations.data(i)
             sample_expl_dict = {}
             sample_expl_dict[self.id_field_name] = ids[i]
-            sample_expl_dict['predicted_class'] = str(class_names[int(local_expl_data["perf"]["predicted"])] )
-            sample_expl_dict['predicted_class_prob'] = np.round(local_expl_data["perf"]["predicted_score"],4)
-            
+            sample_expl_dict["predicted_class"] = str(
+                class_names[int(local_expl_data["perf"]["predicted"])]
+            )
+            sample_expl_dict["predicted_class_prob"] = np.round(
+                local_expl_data["perf"]["predicted_score"], 4
+            )
+
             sample_expl_dict["feature_impacts_per_class"] = {}
-            for j, c in enumerate(class_names): 
-                class_exp_dict = {}   
-                class_exp_dict['Intercept'] =  local_expl_data['extra']['scores'][0][j]
-                
+            for j, c in enumerate(class_names):
+                class_exp_dict = {}
+                class_exp_dict["Intercept"] = local_expl_data["extra"]["scores"][0][j]
+
                 feature_impacts = {
-                    f:np.round(v[j],4) for f,v in zip(local_expl_data["names"], local_expl_data["scores"])
+                    f: np.round(v[j], 4)
+                    for f, v in zip(local_expl_data["names"], local_expl_data["scores"])
                 }
                 class_exp_dict["feature_impacts"] = feature_impacts
-                
+
                 sample_expl_dict["feature_impacts_per_class"][str(c)] = class_exp_dict
-            
-            all_explanations.append(sample_expl_dict)            
+
+            all_explanations.append(sample_expl_dict)
             # pprint.pprint(sample_expl_dict)
-        # ------------------------------------------------------  
+        # ------------------------------------------------------
         all_explanations = json.dumps(all_explanations, cls=utils.NpEncoder, indent=2)
         return all_explanations
